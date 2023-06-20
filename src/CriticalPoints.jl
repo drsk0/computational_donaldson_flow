26350c4f62f6ef4376660ad5d44013d75f7624fd
drsk <drsk@lavabit.com>
2023-06-20 17:26:50 +0200

    critical points: better formulation of equation

--- src/CriticalPoints.jl
+++ src/CriticalPoints.jl
@@ -3,110 +3,100 @@ import ModelingToolkit: Interval
 
 @parameters x0 x1 x2 x3
 @variables ρ01(..) ρ02(..) ρ03(..) ρ12(..) ρ13(..) ρ23(..)
-@variables u(..)
-@variables K[1:3](..)
-@variables A[1:4, 1:4](..)
-@variables AInvPartialK[1:3](..)
-@variables S[1:3, 1:4](..) STot[1:4](..)
 
 ∂₀ = Differential(x0)
-∂₁= Differential(x1)
+∂₁ = Differential(x1)
 ∂₂ = Differential(x2)
-∂₃= Differential(x3)
+∂₃ = Differential(x3)
+
+d(ρ) = [
+    ∂₀(ρ[2]),
+    ∂₀(ρ[3]),
+    ∂₀(ρ[4]),
+    ∂₁(ρ[3]),
+    ∂₁(ρ[4]),
+    ∂₂(ρ[4]),
+]
+
 
 J₁ = [
-    0 -1 0 0;
-    1 0 0 0;
-    0 0 0 -1;
+    0 -1 0 0
+    1 0 0 0
+    0 0 0 -1
     0 0 1 0
 ]
 
 J₂ = [
-    0 0 -1 0;
-    0 0 0 1;
-    1 0 0 0;
+    0 0 -1 0
+    0 0 0 1
+    1 0 0 0
     0 -1 0 0
 ]
 
 J₃ = [
-    0 0 0 -1;
-    0 0 -1 0;
-    0 1 0 0;
+    0 0 0 -1
+    0 0 -1 0
+    0 1 0 0
     1 0 0 0
 ]
 
-A0(ρ01, ρ02, ρ03, ρ12, ρ13, ρ23) = [
-      0   ρ01     ρ02     ρ03;
-   -ρ01     0     ρ12     ρ13;
-   -ρ02  -ρ12       0     ρ23;
-   -ρ03  -ρ13    -ρ23       0;
+u(x0, x1, x2, x3) = ρ01(x0, x1, x2, x3) * ρ23(x0, x1, x2, x3) - ρ02(x0, x1, x2, x3) * ρ13(x0, x1, x2, x3) + ρ03(x0, x1, x2, x3) * ρ12(x0, x1, x2, x3)
+
+K(x0, x1, x2, x3) = [
+    2(ρ01(x0, x1, x2, x3) + ρ23(x0, x1, x2, x3)) / u(x0, x1, x2, x3),
+    2(ρ02(x0, x1, x2, x3) - ρ13(x0, x1, x2, x3)) / u(x0, x1, x2, x3),
+    2(ρ03(x0, x1, x2, x3) + ρ12(x0, x1, x2, x3)) / u(x0, x1, x2, x3)
 ]
 
-#critical point equation
-eqs = [ 
-    u(x0,x1,x2,x3) ~ ρ01(x0,x1,x2,x3) * ρ23(x0,x1,x2,x3) + ρ02(x0,x1,x2,x3)*ρ31(x0,x1,x2,x3) + ρ03(x0,x1,x2,x3)*ρ12(x0,x1,x2,x3),
-    K[1](x0,x1,x2,x3) ~ 2(ρ01(x0,x1,x2,x3) + ρ23(x0,x1,x2,x3)) / u(x0,x1,x2,x3),
-    K[2](x0,x1,x2,x3) ~ 2(ρ02(x0,x1,x2,x3) - ρ31(x0,x1,x2,x3)) / u(x0,x1,x2,x3),
-    K[3](x0,x1,x2,x3) ~ 2(ρ03(x0,x1,x2,x3) + ρ12(x0,x1,x2,x3)) / u(x0,x1,x2,x3),
-    A(x0, x1, x2, x3) ~ A0(ρ01(x0,x1,x2,x3), ρ02(x0,x1,x2,x3), ρ03(x0,x1,x2,x3), ρ12(x0,x1,x2,x3), ρ13(x0,x1,x2,x3), ρ23(x0,x1,x2,x3)),
-    A(x0, x1, x2, x3) * AInvPartialK[1](x0, x1, x2, x3) ~ [∂₀(K[1](x0,x1,x2,x3)), ∂₁(K[1](x0,x1,x2,x3)), ∂₂(K[1](x0,x1,x2,x3)), ∂₃(K[1](x0,x1,x2,x3))],
-    A(x0, x1, x2, x3) * AInvPartialK[2](x0, x1, x2, x3) ~ [∂₀(K[2](x0,x1,x2,x3)), ∂₁(K[2](x0,x1,x2,x3)), ∂₂(K[2](x0,x1,x2,x3)), ∂₃(K[2](x0,x1,x2,x3))],
-    A(x0, x1, x2, x3) * AInvPartialK[3](x0, x1, x2, x3) ~ [∂₀(K[3](x0,x1,x2,x3)), ∂₁(K[3](x0,x1,x2,x3)), ∂₂(K[3](x0,x1,x2,x3)), ∂₃(K[3](x0,x1,x2,x3))],
-    S[1](x0,x1,x2,x3) ~ A(x0, x1, x2, x3) * J₁ * AInvPartialK[1](x0, x1, x2, x3),
-    S[2](x0,x1,x2,x3) ~ A(x0, x1, x2, x3) * J₂ * AInvPartialK[2](x0, x1, x2, x3),
-    S[3](x0,x1,x2,x3) ~ A(x0, x1, x2, x3) * J₃ * AInvPartialK[3](x0, x1, x2, x3),
-    # alternatively solve the inverse problem directly.
-    # S[1](x0, x1, x2, x3) ~ A(x0, x1, x2, x3) * J₁ * [∂₀(K[1](x0,x1,x2,x3)), ∂₁(K[1](x0,x1,x2,x3)), ∂₂(K[1](x0,x1,x2,x3)), ∂₃(K[1](x0,x1,x2,x3))] / A(x0,x1,x2,x3),
-    # S[2](x0, x1, x2, x3) ~ A(x0, x1, x2, x3) * J₂ * [∂₀(K[2](x0,x1,x2,x3)), ∂₁(K[2](x0,x1,x2,x3)), ∂₂(K[2](x0,x1,x2,x3)), ∂₃(K[2](x0,x1,x2,x3))] / A(x0,x1,x2,x3),
-    # S[3](x0, x1, x2, x3) ~ A(x0, x1, x2, x3) * J₃ * [∂₀(K[3](x0,x1,x2,x3)), ∂₁(K[3](x0,x1,x2,x3)), ∂₂(K[3](x0,x1,x2,x3)), ∂₃(K[3](x0,x1,x2,x3))] / A(x0,x1,x2,x3),
-    STot(x0,x1,x2,x3) ~ sum(S[:](x0,x1,x2,x3)),
-    0.0 ~ ∂₀(STot[2](x0,x1,x2,x3)),
-    0.0 ~ ∂₀(STot[3](x0,x1,x2,x3)),
-    0.0 ~ ∂₀(STot[4](x0,x1,x2,x3)),
-    0.0 ~ ∂₁(STot[3](x0,x1,x2,x3)),
-    0.0 ~ ∂₁(STot[4](x0,x1,x2,x3)),
-    0.0 ~ ∂₂(STot[4](x0,x1,x2,x3)),
+A(x0, x1, x2, x3) = [
+    0 ρ01(x0, x1, x2, x3) ρ02(x0, x1, x2, x3) ρ03(x0, x1, x2, x3)
+    -ρ01(x0, x1, x2, x3) 0 ρ12(x0, x1, x2, x3) ρ13(x0, x1, x2, x3)
+    -ρ02(x0, x1, x2, x3) -ρ12(x0, x1, x2, x3) 0 ρ23(x0, x1, x2, x3)
+    -ρ03(x0, x1, x2, x3) -ρ13(x0, x1, x2, x3) -ρ23(x0, x1, x2, x3) 0
 ]
+
+▿E(x0, x1, x2, x3) = d(A(x0, x1, x2, x3) * J₁ * A(x0, x1, x2, x3) \ [∂₀(K(x0, x1, x2, x3)[1]), ∂₁(K(x0, x1, x2, x3)[1]), ∂₂(K(x0, x1, x2, x3)[1]), ∂₃(K(x0, x1, x2, x3)[1])]
+                       + A(x0, x1, x2, x3) * J₂ * A(x0, x1, x2, x3) \ [∂₀(K(x0, x1, x2, x3)[2]), ∂₁(K(x0, x1, x2, x3)[2]), ∂₂(K(x0, x1, x2, x3)[2]), ∂₃(K(x0, x1, x2, x3)[2])]
+                       + A(x0, x1, x2, x3) * J₃ * A(x0, x1, x2, x3) \ [∂₀(K(x0, x1, x2, x3)[3]), ∂₁(K(x0, x1, x2, x3)[3]), ∂₂(K(x0, x1, x2, x3)[3]), ∂₃(K(x0, x1, x2, x3)[3])]
+)
+
+#critical point equation
+eqs = [▿E(x0, x1, x2, x3) ~ zeros(6)]
 
 # periodic boundary conditions for the 4-torus
 bcs = [
-    ρ01(0.0,x1,x2,x3) ~ ρ01(2π,x1,x2,x3),
-    ρ01(x0,0.0,x2,x3) ~ ρ01(x0,2π,x2,x3),
-    ρ01(x0,x1,0.0,x3) ~ ρ01(x0,x1,2π,x3),
-    ρ01(x0,x1,x2,0.0) ~ ρ01(x0,x1,x2,2π),
-
-    ρ02(0.0,x1,x2,x3) ~ ρ02(2π,x1,x2,x3),
-    ρ02(x0,0.0,x2,x3) ~ ρ02(x0,2π,x2,x3),
-    ρ02(x0,x1,0.0,x3) ~ ρ02(x0,x1,2π,x3),
-    ρ02(x0,x1,x2,0.0) ~ ρ02(x0,x1,x2,2π),
-
-    ρ03(0.0,x1,x2,x3) ~ ρ03(2π, x1,x2,x3),
-    ρ03(x0,0.0,x2,x3) ~ ρ03(x0,2π,x2,x3),
-    ρ03(x0,x1,0.0,x3) ~ ρ03(x0,x1,2π,x3),
-    ρ03(x0,x1,x2,0.0) ~ ρ03(x0,x1,x2,2π),
-
-    ρ12(0.0,x1,x2,x3) ~ ρ12(2π,x1,x2,x3),
-    ρ12(x0,0.0,x2,x3) ~ ρ12(x0,2π,x2,x3),
-    ρ12(x0,x1,0.0,x3) ~ ρ12(x0,x1,2π,x3),
-    ρ12(x0,x1,x2,0.0) ~ ρ12(x0,x1,x2,2π),
-
-    ρ13(0.0,x1,x2,x3) ~ ρ13(2π,x1,x2,x3),
-    ρ13(x0,0.0,x2,x3) ~ ρ13(x0,2π,x2,x3),
-    ρ13(x0,x1,0.0,x3) ~ ρ13(x0,x1,2π,x3),
-    ρ13(x0,x1,x2,0.0) ~ ρ13(x0,x1,x2,2π),
-
-    ρ23(0.0,x1,x2,x3) ~ ρ23(2π,x1,x2,x3),
-    ρ23(x0,0.0,x2,x3) ~ ρ23(x0,2π,x2,x3),
-    ρ23(x0,x1,0.0,x3) ~ ρ23(x0,x1,2π,x3),
-    ρ23(x0,x1,x2,0.0) ~ ρ23(x0,x1,x2,2π)
+    ρ01(0.0, x1, x2, x3) ~ ρ01(2π, x1, x2, x3),
+    ρ01(x0, 0.0, x2, x3) ~ ρ01(x0, 2π, x2, x3),
+    ρ01(x0, x1, 0.0, x3) ~ ρ01(x0, x1, 2π, x3),
+    ρ01(x0, x1, x2, 0.0) ~ ρ01(x0, x1, x2, 2π), 
+    ρ02(0.0, x1, x2, x3) ~ ρ02(2π, x1, x2, x3),
+    ρ02(x0, 0.0, x2, x3) ~ ρ02(x0, 2π, x2, x3),
+    ρ02(x0, x1, 0.0, x3) ~ ρ02(x0, x1, 2π, x3),
+    ρ02(x0, x1, x2, 0.0) ~ ρ02(x0, x1, x2, 2π), 
+    ρ03(0.0, x1, x2, x3) ~ ρ03(2π, x1, x2, x3),
+    ρ03(x0, 0.0, x2, x3) ~ ρ03(x0, 2π, x2, x3),
+    ρ03(x0, x1, 0.0, x3) ~ ρ03(x0, x1, 2π, x3),
+    ρ03(x0, x1, x2, 0.0) ~ ρ03(x0, x1, x2, 2π), 
+    ρ12(0.0, x1, x2, x3) ~ ρ12(2π, x1, x2, x3),
+    ρ12(x0, 0.0, x2, x3) ~ ρ12(x0, 2π, x2, x3),
+    ρ12(x0, x1, 0.0, x3) ~ ρ12(x0, x1, 2π, x3),
+    ρ12(x0, x1, x2, 0.0) ~ ρ12(x0, x1, x2, 2π), 
+    ρ13(0.0, x1, x2, x3) ~ ρ13(2π, x1, x2, x3),
+    ρ13(x0, 0.0, x2, x3) ~ ρ13(x0, 2π, x2, x3),
+    ρ13(x0, x1, 0.0, x3) ~ ρ13(x0, x1, 2π, x3),
+    ρ13(x0, x1, x2, 0.0) ~ ρ13(x0, x1, x2, 2π), 
+    ρ23(0.0, x1, x2, x3) ~ ρ23(2π, x1, x2, x3),
+    ρ23(x0, 0.0, x2, x3) ~ ρ23(x0, 2π, x2, x3),
+    ρ23(x0, x1, 0.0, x3) ~ ρ23(x0, x1, 2π, x3),
+    ρ23(x0, x1, x2, 0.0) ~ ρ23(x0, x1, x2, 2π)
 ]
 
-# the 4-torus
+# # the 4-torus
 domains = [
-    x0 ∈ Interval(0,2π),
-    x1 ∈ Interval(0,2π),
-    x2 ∈ Interval(0,2π),
-    x3 ∈ Interval(0,2π),
+    x0 ∈ Interval(0, 2π),
+    x1 ∈ Interval(0, 2π),
+    x2 ∈ Interval(0, 2π),
+    x3 ∈ Interval(0, 2π),
 ]
 
 input_ = length(domains)
@@ -115,7 +105,7 @@ chain = [Lux.Chain(Dense(input_, n, Lux.σ), Dense(n, n, Lux.σ), Dense(n, 1)) f
 
 strategy = QuadratureTraining()
 discretization = PhysicsInformedNN(chain, strategy)
-@named pdesystem = PDESystem(eqs, bcs, domains, [x0,x1,x2,x3], [ρ01(x0,x1,x2,x3), ρ02(x0,x1,x2,x3), ρ03(x0,x1,x2,x3), ρ12(x0,x1,x2,x3), ρ13(x0,x1,x2,x3), ρ23(x0,x1,x2,x3)])
+@named pdesystem = PDESystem(eqs, bcs, domains, [x0, x1, x2, x3], [ρ01(x0, x1, x2, x3), ρ02(x0, x1, x2, x3), ρ03(x0, x1, x2, x3), ρ12(x0, x1, x2, x3), ρ13(x0, x1, x2, x3), ρ23(x0, x1, x2, x3)])
 prob = discretize(pdesystem, discretization)
 sym_prob = symbolic_discretize(pdesystem, discretization)
 
@@ -130,4 +120,4 @@ callback = function (p, l)
 end
 
 
-res = Optimization.solve(prob, BFGS(); callback = callback, maxiters = 5000)
+run() = Optimization.solve(prob, BFGS(); callback=callback, maxiters=5000)

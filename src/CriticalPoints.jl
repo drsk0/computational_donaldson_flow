7f26181e9e6c6e082ad7cd7288d862c1f6bdaca7
drsk <drsk@lavabit.com>
2023-06-20 12:37:44 +0200

    critical points: first solver complete

--- src/CriticalPoints.jl
+++ src/CriticalPoints.jl
@@ -43,7 +43,7 @@ A0(ρ01, ρ02, ρ03, ρ12, ρ13, ρ23) = [
 ]
 
 #critical point equation
-eq = [ 
+eqs = [ 
     u(x0,x1,x2,x3) ~ ρ01(x0,x1,x2,x3) * ρ23(x0,x1,x2,x3) + ρ02(x0,x1,x2,x3)*ρ31(x0,x1,x2,x3) + ρ03(x0,x1,x2,x3)*ρ12(x0,x1,x2,x3),
     K[1](x0,x1,x2,x3) ~ 2(ρ01(x0,x1,x2,x3) + ρ23(x0,x1,x2,x3)) / u(x0,x1,x2,x3),
     K[2](x0,x1,x2,x3) ~ 2(ρ02(x0,x1,x2,x3) - ρ31(x0,x1,x2,x3)) / u(x0,x1,x2,x3),
@@ -100,3 +100,34 @@ bcs = [
     ρ23(x0,x1,0.0,x3) ~ ρ23(x0,x1,2π,x3),
     ρ23(x0,x1,x2,0.0) ~ ρ23(x0,x1,x2,2π)
 ]
+
+# the 4-torus
+domains = [
+    x0 ∈ Interval(0,2π),
+    x1 ∈ Interval(0,2π),
+    x2 ∈ Interval(0,2π),
+    x3 ∈ Interval(0,2π),
+]
+
+input_ = length(domains)
+n = 15
+chain = [Lux.Chain(Dense(input_, n, Lux.σ), Dense(n, n, Lux.σ), Dense(n, 1)) for _ in 1:6]
+
+strategy = QuadratureTraining()
+discretization = PhysicsInformedNN(chain, strategy)
+@named pdesystem = PDESystem(eqs, bcs, domains, [x0,x1,x2,x3], [ρ01(x0,x1,x2,x3), ρ02(x0,x1,x2,x3), ρ03(x0,x1,x2,x3), ρ12(x0,x1,x2,x3), ρ13(x0,x1,x2,x3), ρ23(x0,x1,x2,x3)])
+prob = discretize(pdesystem, discretization)
+sym_prob = symbolic_discretize(pdesystem, discretization)
+
+pde_inner_loss_functions = sym_prob.loss_functions.pde_loss_functions
+bcs_inner_loss_functions = sym_prob.loss_functions.bc_loss_functions
+
+callback = function (p, l)
+    println("loss: ", l)
+    println("pde_losses: ", map(l_ -> l_(p), pde_inner_loss_functions))
+    println("bcs_losses: ", map(l_ -> l_(p), bcs_inner_loss_functions))
+    return false
+end
+
+
+res = Optimization.solve(prob, BFGS(); callback = callback, maxiters = 5000)
